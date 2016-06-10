584852aaa5dcebed9256a6c29afd844a2d792268
Barret Schloerke <schloerke@gmail.com>
2016-06-10 15:28:23 -0400

    finished up matching the ghpages in tests

--- tests/testthat/test_ghpages.R
+++ tests/testthat/test_ghpages.R
@@ -1,7 +1,31 @@
 
 
-context("working with data")
+get_cached_data <- function(name, fn) {
+  file_name <- file.path("data", paste(name, ".Rda", sep = ""))
+  if (!file.exists(file_name)) {
+    result <- fn()
+    save(result, file = file_name)
+  }
+  env <- environment()
+  load(file_name, envir = env)
+  get("result", env)
+}
 
+facefit_fn <- function() {
+  get_cached_data("facefit_obj", function() {
+    smc <- get_smocc_data()[1:2000, ]
+
+    get_fit(smc, y_var = "haz", method = "face")
+  })
+}
+
+smc_tr_fn <- function() {
+  get_cached_data("smc_tr", function() {
+    smc <- get_smocc_data()[1:2000, ]
+    facefit <- facefit_fn() # nolint
+    fit_all_trajectories(smc, facefit)
+  })
+}
 
 expect_class <- function(x, class) {
   expect_true(inherits(x, class)) # nolint
@@ -10,6 +34,34 @@ expect_class <- function(x, class) {
 expect_data_frame <- function(x, colnames) {
   expect_class(x, "data.frame") # nolint
   expect_equivalent(colnames(x), colnames)
+}
+
+expect_silent_plot <- function(p) {
+  pdf(file = NULL)
+  expect_silent({
+    print(p)
+  })
+  dev.off()
+}
+expect_warning_plot <- function(p, warning) {
+  pdf(file = NULL)
+  expect_warning({
+    print(p)
+  }, warning) # nolint
+  dev.off()
+}
+
+expect_fit_plot <- function(p) {
+  expect_rbokeh(p, c(
+    "Patches", "Patches", "Patches", "Line", "Circle",
+    "Line", "Circle", "CircleX"
+  ))
+}
+expect_fit_zplot <- function(p) {
+  expect_rbokeh(p, c(
+    "Patches", "Patches", "Patches", "Patches", "Line",
+    "Circle", "Line", "Circle", "CircleX"
+  ))
 }
 
 expect_rbokeh <- function(p, layer_types) {
@@ -25,17 +77,61 @@ expect_rbokeh <- function(p, layer_types) {
     unlist() %>% unname() ->
   actual_layer_types
 
-
   expect_equivalent(layer_types, actual_layer_types) # nolint
 
-  json_output <- capture.output({
-    print_model_json(p)
-  })
-  expect_class(json_output, "character") # nolint
-  expect_equivalent(json_output[4], "    \"title\": \"Bokeh Figure\",") # nolint
-  expect_true(length(json_output) > 500) # nolint
+  # json_output <- capture.output({
+  #   print_model_json(p)
+  # })
+  # expect_class(json_output, "character") # nolint
+  # expect_equivalent(json_output[4], "    \"title\": \"Bokeh Figure\",") # nolint
+  # expect_true(length(json_output) > 500) # nolint
 }
 
+expect_rbokeh_plot_matrix <- function(pm, type_list, ncol, check_data = NULL) {
+
+  last <- function(x) {
+    x[[length(x)]]
+  }
+
+  expect_class(pm$x$spec, "BokehGridPlot") # nolint
+
+  if (!is.null(check_data)) {
+    check_data_cols <- colnames(check_data)
+    num_check_data_cols <- length(check_data_cols)
+
+    expect_equivalent(length(pm$x$spec$plot_refs), ceiling(num_check_data_cols / ncol))
+
+    expect_true(
+      ! is.null(
+        last(pm$x$spec$plot_refs)[
+          last(rep(1:ncol, length.out = num_check_data_cols))
+        ]
+      )
+    )
+  }
+
+
+  pm$x$spec$plot_refs %>%
+    unlist(recursive = FALSE) %>%
+    lapply("[[", "id") %>%
+    unlist() %>% unname() ->
+  plot_ids
+
+  seq_along(plot_ids) %>%
+    lapply(function(i) {
+      p <- pm$x$spec$figs[[plot_ids[i]]]
+      expect_rbokeh(p, type_list[[i]]) # nolint
+
+      if (!is.null(check_data)) {
+        expect_equivalent(p$x$spec$model$plot$attributes$title, check_data_cols[i])
+      }
+    })
+}
+
+
+
+
+context("working with data")
 
 test_that("data", {
   expect_data_frame(cpp, c(
@@ -340,9 +436,7 @@ test_that("ggplot2", {
   expect_layer_type(who_p, 7, "GeomPath")
   expect_layer_type(who_p, 8, "GeomPoint")
 
-  expect_warning({
-    print(who_p)
-  }, "Removed 1 rows containing missing values") # nolint
+  expect_warning_plot(who_p, "Removed 1 rows containing missing values")
 
 })
 
@@ -366,9 +460,7 @@ test_that("lattice", {
     col = "black"
   )
 
-  expect_silent({
-    print(lat_p)
-  })
+  expect_silent_plot(lat_p)
 
 })
 
@@ -418,56 +510,221 @@ test_that("rbokeh", {
 
 context("Simple Exploration")
 
-test_that("plot univar", {
+test_that("Data distributions", {
 
-  expect_rbokeh_plot_matrix <- function(pm, ncol, check_data) {
-
-    last <- function(x) {
-      x[[length(x)]]
-    }
-
-    expect_class(pm$x$spec, "BokehGridPlot")
-
-    subject_cpp <- get_subject_data(cpp)
-
-    check_data_cols <- colnames(check_data)
-    num_check_data_cols <- length(check_data_cols)
-
-    expect_equivalent(length(pm$x$spec$plot_refs), ceiling(num_check_data_cols / ncol))
-
-    expect_true(
-      ! is.null(
-        last(pm$x$spec$plot_refs)[
-          last(rep(1:ncol, length.out = num_check_data_cols))
-        ]
-      )
-    )
-
-    pm$x$spec$plot_refs %>%
-      unlist(recursive = FALSE) %>%
-      lapply("[[", "id") %>%
-      unlist() %>% unname() ->
-    plot_ids
-
-    pm$x$spec$figs[plot_ids] %>%
-      lapply(expect_rbokeh, c("Quad"))
-
-    seq_along(plot_ids) %>%
-      lapply(function(i) {
-        p <- pm$x$spec$figs[[plot_ids[i]]]
-        expect_equivalent(p$x$spec$model$plot$attributes$title, check_data_cols[i])
-      })
-  }
 
   expect_rbokeh_plot_matrix(
     plot_univar(cpp, subject = TRUE, width = 250, height = 250, ncol = 3),
+    rep("Quad", 25),
     3,
     get_subject_data(cpp)
   )
   expect_rbokeh_plot_matrix(
     plot_univar(cpp, subject = FALSE, width = 250, height = 250, ncol = 4),
+    rep("Quad", 12),
     4,
     get_time_data(cpp)
   )
 
+})
+
+
+test_that("Missing data", {
+  p <- plot_missing(cpp, subject = TRUE)
+  expect_rbokeh(p, "Quad")
+
+  p <- plot_missing(cpp, subject = FALSE)
+  expect_rbokeh(p, "Quad")
+
+  p <- plot_complete_pairs(cpp, subject = TRUE)
+  expect_rbokeh(p, "Rect")
+
+  p <- plot_complete_pairs(cpp, subject = FALSE)
+  expect_rbokeh(p, "Rect")
+})
+
+test_that("special plots", {
+  expect_rbokeh_plot_matrix(
+    plot_visit_distn(cpp, width = 350, height = 350),
+    c("Quad", "Circle"),
+    2
+  )
+
+  expect_rbokeh_plot_matrix(
+    plot_first_visit_age(cpp, width = 350, height = 350),
+    c("Quad", "Circle"),
+    2
+  )
+
+
+  agefreq <- get_agefreq(cpp)
+  p <- plot_agefreq(agefreq)
+  expect_rbokeh(p, "Line")
+
+})
+
+
+context("Modeling Methods")
+
+test_that("fitting a model to a data set", {
+  smc <- get_smocc_data()[1:2000, ]
+
+  for (meth in get_avail_methods()) {
+    y_var <- switch(meth,
+      "sitar" = "htcm",
+      "haz"
+      )
+    fit_obj <- get_cached_data(
+      paste("fit", meth, sep = "_"),
+      function() {
+        get_fit(smc, y_var = y_var, method = meth)
+      }
+    )
+
+    expect_output(
+      print(fit_obj),
+      paste("Object obtained from get_fit\\(\\) using method '", meth, "'", sep = "")
+    )
+  }
+
+
+  facefit <- facefit_fn()
+
+  fit <- get_cached_data("fit_obj", function() {
+    fit_trajectory(subset(smc, subjid == 10001), facefit)
+  })
+
+  expect_fit_plot(plot(fit, x_range = c(0, 755), hover = c("agedays", "htcm")))
+  expect_fit_plot(plot(fit, x_range = c(0, 755), hover = c("agedays", "htcm"), center = TRUE))
+
+  expect_fit_zplot(plot_z(fit, hover = c("agedays", "wtkg")))
+  expect_rbokeh(plot_velocity(fit), c("Line"))
+  expect_rbokeh(plot_zvelocity(fit), c("Line"))
+})
+
+test_that("fit all trajectories", {
+  facefit <- facefit_fn()
+  smc_tr <- smc_tr_fn()
+
+  expect_fit_plot(
+    plot(smc_tr[[1]]$value, hover = c("agedays", "htcm"))
+  )
+
+  expect_fit_zplot(
+    plot_z(smc_tr[["subjid=10002"]]$value, hover = c("agedays", "htcm"))
+  )
+})
+
+test_that("Assessing fits with holdouts", {
+  smc <- get_smocc_data()[1:2000, ]
+
+  set.seed(1234)
+  smc2 <- add_holdout_ind(smc)
+
+  mse_ans <- list(
+    "brokenstick" = 0.1798087,
+    "face" = 0.1170519,
+    "fda" = NA,
+    "gam" = 0.3668065,
+    "loess" = NA,
+    "lwmod" = 0.3159714,
+    "rlm" = 0.4613727,
+    "sitar" = NA,
+    "smooth.spline" = 0.432525,
+    "wand" = 0.2637936
+  )
+  for (meth in get_avail_methods()) {
+    if (meth %in% c("fda", "loess", "sitar")) {
+      next
+    }
+    y_var <- switch(meth,
+      "sitar" = "htcm",
+      "haz"
+    )
+
+    smc_tr_mse <- get_cached_data(
+      paste("holdout", meth, sep = "_"),
+      function() {
+        fit_hold <- get_fit(
+          smc2,
+          y_var = y_var,
+          method = meth,
+          holdout = TRUE
+        )
+        smc_tr_hold <- fit_all_trajectories(smc2, fit_hold)
+        get_fit_holdout_mse(smc_tr_hold)
+      }
+    )
+
+    expect_equivalent(round(smc_tr_mse, 6), round(mse_ans[[meth]], 6))
+
+  }
+
+})
+
+context("Trelliscope")
+
+test_that("built-in methods", {
+
+  smc_tr <- smc_tr_fn()
+
+  vdb_path <- "ghap_vdb"
+
+  test_vdb_conn <- vdbConn(name = "ghap_vdb", path = vdb_path, autoYes = TRUE) # nolint
+
+  trscope_trajectories(smc_tr, center = TRUE, vdbConn = test_vdb_conn)
+  trscope_trajectories(smc_tr, z = TRUE, vdbConn = test_vdb_conn)
+  trscope_velocities(smc_tr, vdbConn = test_vdb_conn)
+  trscope_velocities(smc_tr, z = TRUE, vdbConn = test_vdb_conn)
+
+  unlink(vdb_path, recursive = TRUE)
+
+  expect_true(TRUE)
+  # trscope_trajectories(smc_tr)
+})
+
+
+context("Division Methods")
+
+test_that("By Subject", {
+  smc_tr <- smc_tr_fn()
+
+  smc_cp <- by_trajectory_checkpoints(smc_tr)
+
+  expect_equivalent(length(getKeys(smc_cp)), 1)
+})
+
+
+context("Multi-Dataset Vis")
+
+test_that("multi dataset vis", {
+  dat_list <- list(cpp = cpp, smc = get_smocc_data()[1:2000, ])
+
+  expect_rbokeh(plot_var_matrix(dat_list), c("Rect"))
+
+  expect_rbokeh_plot_matrix(
+    plot_time_count_grid(dat_list),
+    c("Quad", "Quad"),
+    1
+  )
+
+  expect_rbokeh(
+    plot_multi_subj_boxplot(dat_list),
+    c("Rect", "Segment", "Circle", "Rect", "Segment", "Circle")
+  )
+})
+
+
+context("Misc")
+
+test_that("size conversion", {
+  expect_equivalent(
+    round(who_htcm2zscore(1670, in2cm(44)), 6),
+    round(1.117365, 6)
+  )
+
+  expect_equivalent(
+    round(who_wtkg2zscore(1670, lb2kg(48)), 6),
+    round(1.527048, 6)
+  )
 })
