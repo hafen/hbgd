229337a039114d935652013689f86bbe9f4955ec
Barret Schloerke <schloerke@gmail.com>
2016-07-20 17:26:34 -0400

    Remove silly example

--- R/hbgd-package.R
+++ R/hbgd-package.R
@@ -13,8 +13,8 @@ utils::globalVariables(c(
 #' @import datadr
 #' @import trelliscope
 #' @importFrom rbokeh figure ly_lines ly_points ly_rect ly_crect ly_hist ly_quantile grid_plot theme_axis x_axis ly_bar theme_grid tool_wheel_zoom tool_pan theme_plot ly_text pal_tableau
-#' @examples
-#' help(package = hbgd)
+# ' @examples
+# ' help(package = hbgd)
 #' @importFrom dplyr group_by summarise mutate arrange filter desc summarise_each group_by_ n_distinct n funs
 #' @importFrom grDevices colorRampPalette
 #' @importFrom graphics plot
